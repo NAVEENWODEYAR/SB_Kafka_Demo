7a4665a8c6433b8b28fec868c7af18ad4dddb8e4
Naveen <naveenkrishnawodeyar@gmail.com>
2024-07-30 20:36:47 +0530

    added 1 more listener to the topic

--- delivery_app_demo/enduser_app/src/main/java/com/enduser/constants/AppConstants.java
+++ delivery_app_demo/enduser_app/src/main/java/com/enduser/constants/AppConstants.java
@@ -3,6 +3,8 @@ package com.enduser.constants;
 public class AppConstants {
 
     public static final String LOCATION_UPDATE_TOPIC = "location-update-topic";
+    public static final String LOCATION_UPDATE_TOPIC1 = "location-update-topic";
 
     public static final String GROUP_ID = "group-1";
+    public static final String GROUP_ID1 = "group-1";
 }
